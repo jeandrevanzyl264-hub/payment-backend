0d994a1923d1d90fc37b7430bcce83b46f76c942
Jeandre Van Zyl <nicolekrotz664@gmail.com>
2026-08-20 18:52:01 +0200

    Fix syntax error in app.listen

--- server.js
+++ server.js
@@ -143,4 +143,4 @@ app.get('/api/orders', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0' () => console.log(`Server running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
